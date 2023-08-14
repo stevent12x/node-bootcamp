8add69e0e870cc0deb662cf05ae213cc0d85d83d
stevent12-rg <sturner@rategenius.com>
2023-08-14 17:33:24 -0500

    install prettier

--- 1-node-farm/starter/modules/replaceTemplate.js
+++ 1-node-farm/starter/modules/replaceTemplate.js
@@ -1,29 +1,27 @@
 module.exports = {
-    replaceTemplate,
-    header
-}
+	replaceTemplate,
+	header
+};
 
 function replaceTemplate(template, product) {
-    let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRODUCTPRICE%}/g, product.price);
-    output = output.replace(/{%PRODUCTLOCATION%}/g, product.from);
-    output = output.replace(/{%PRODUCTNUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%PRODUCTQUANTITY%}/g, product.quantity);
-    output = output.replace(/{%PRODUCTDESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-    output = output.replace(/{%SLUG%}/g, product.slug);
+	let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
+	output = output.replace(/{%IMAGE%}/g, product.image);
+	output = output.replace(/{%PRODUCTPRICE%}/g, product.price);
+	output = output.replace(/{%PRODUCTLOCATION%}/g, product.from);
+	output = output.replace(/{%PRODUCTNUTRIENTS%}/g, product.nutrients);
+	output = output.replace(/{%PRODUCTQUANTITY%}/g, product.quantity);
+	output = output.replace(/{%PRODUCTDESCRIPTION%}/g, product.description);
+	output = output.replace(/{%ID%}/g, product.id);
+	output = output.replace(/{%SLUG%}/g, product.slug);
 
-    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+	if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
 
-    return output;
+	return output;
 }
 
 function header(res) {
-    return res.writeHead(
-        200, {
-            'Content-type': 'text/html',
-            'my-own-header': 'node-farm'
-        }
-    );
+	return res.writeHead(200, {
+		'Content-type': 'text/html',
+		'my-own-header': 'node-farm'
+	});
 }
