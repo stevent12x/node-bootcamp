9859c006d170128152e4ae803b2b24c0801927bb
stevent12-rg <sturner@rategenius.com>
2023-08-14 17:31:47 -0500

    install prettier

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 const slugify = require('slugify');
-const helpers = require('./modules/replaceTemplate')
+const helpers = require('./modules/replaceTemplate');
 
 ////////////////////
 //FILES
@@ -39,47 +39,49 @@ const templateProduct = fs.readFileSync(`${__dirname}/templates/template_product
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObject = JSON.parse(data);
-dataObject.map(product => {
-    product['slug'] = slugify(product.productName.toLowerCase());
+dataObject.map((product) => {
+	product['slug'] = slugify(product.productName.toLowerCase());
 });
 
 const server = http.createServer((req, res) => {
-    const {query, pathname} = url.parse(req.url, true);
-    //Overview Page
-    if (pathname === '/' || pathname === '/overview') {
-        helpers.header(res);
+	const { query, pathname } = url.parse(req.url, true);
+	//Overview Page
+	if (pathname === '/' || pathname === '/overview') {
+		helpers.header(res);
 
-        const cardsHTML = dataObject.map(el => helpers.replaceTemplate(templateCard, el)).join('');
-        const output = templateOverview.replace('{%PRODUCT_CARDS%}', cardsHTML);
+		const cardsHTML = dataObject.map((el) => helpers.replaceTemplate(templateCard, el)).join('');
+		const output = templateOverview.replace('{%PRODUCT_CARDS%}', cardsHTML);
 
-        res.end(output);
+		res.end(output);
 
-    //Product Page
-    } else if (pathname.includes('/product')) {
-        helpers.header(res);
+		//Product Page
+	} else if (pathname.includes('/product')) {
+		helpers.header(res);
 
-        const product = dataObject.find(product => {
-            return product.slug === pathname.replace('/product/', '')
-        });
+		const product = dataObject.find((product) => {
+			return product.slug === pathname.replace('/product/', '');
+		});
 
-        const output = helpers.replaceTemplate(templateProduct, product);
-        res.end(output);
+		const output = helpers.replaceTemplate(templateProduct, product);
+		res.end(output);
 
-    //API
-    } else if (pathname === '/api') {
-        res.writeHead(200, {'Content-type': 'application/json'});
-        res.end(data);
+		//API
+	} else if (pathname === '/api') {
+		res.writeHead(200, {
+			'Content-type': 'application/json'
+		});
+		res.end(data);
 
-    //Not Found
-    } else {
-        res.writeHead(404, {
-            'Content-type': 'text/html',
-            'my-own-header': 'hello-world'
-        });
-        res.end('<h1>Page NOT FOUND</h1>');
-    }
+		//Not Found
+	} else {
+		res.writeHead(404, {
+			'Content-type': 'text/html',
+			'my-own-header': 'hello-world'
+		});
+		res.end('<h1>Page NOT FOUND</h1>');
+	}
 });
 
 server.listen(8000, '127.0.0.1', () => {
-    console.log('Listening to request on port 8000');
+	console.log('Listening to request on port 8000');
 });
