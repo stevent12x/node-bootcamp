d3e30db74a9794383307a98605cf27e8918f271d
stevent12-rg <sturner@rategenius.com>
2023-08-30 08:37:41 -0500

    Section 4 - connect to mongodb, update gitignore

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,6 +1,52 @@
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
+const mongoose = require('mongoose');
+
+const DB = process.env.DB.replace('<PASSWORD>', process.env.DB_PASSWORD);
+mongoose
+   .connect(DB, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true
+   })
+   .then(() => {
+      console.log('DB connection successful');
+   });
+
+const tourSchema = new mongoose.Schema({
+   name: {
+      type: String,
+      required: [true, 'Tour name is required'],
+      unique: true
+   },
+   rating: {
+      type: Number,
+      default: 4.5
+   },
+   price: {
+      type: Number,
+      required: [true, 'Tour price is required']
+   }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+   name: 'The Park Camper',
+   rating: 4.9,
+   price: 997
+});
+
+testTour
+   .save()
+   .then(doc => {
+      console.log(doc);
+   })
+   .catch(err => {
+      console.log('ERROR', err);
+   });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
