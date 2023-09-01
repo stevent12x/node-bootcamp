ad32217a12c53d1ca50f784a860c39ca76c395ce
stevent12-rg <sturner@rategenius.com>
2023-09-01 07:08:31 -0500

    Section 4 - import dev data script

---
+++ 4-natours/starter/dev-data/data/import-dev-data.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const fs = require('fs');
+const tourModel = require('./../../model/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DB.replace('<PASSWORD>', process.env.DB_PASSWORD);
+
+mongoose
+   .connect(DB, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true
+   })
+   .then(() => console.log('DB connection successful'));
+
+// Read JSON file
+const tours = JSON.parse(
+   fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+// Import data into DB
+const importData = async () => {
+   try {
+      await tourModel.Tour.create(tours);
+      console.log('Successfully imported!');
+   } catch (err) {
+      console.log(err);
+   }
+   process.exit();
+};
+
+// Delete all data from collection
+const deleteData = async () => {
+   try {
+      await tourModel.Tour.deleteMany();
+      console.log('Successfully deleted!');
+   } catch (err) {
+      console.log(err);
+   }
+   process.exit();
+};
+
+if (process.argv[2] === '--import') {
+   importData();
+} else if (process.argv[2] === '--delete') {
+   deleteData();
+}
