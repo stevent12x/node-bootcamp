96bd6231c0fd8f06b45939410f0c4945e992a104
stevent12-rg <sturner@rategenius.com>
2023-09-10 12:26:58 -0500

    Section 9 - validation, cast, and duplicate error handling

--- 4-natours/starter/controller/errorController.js
+++ 4-natours/starter/controller/errorController.js
@@ -1,12 +1,60 @@
+const AppError = require('../util/appError');
+
+const handleCastErrorDB = err => {
+   const message = `Invalid ${err.path}: ${err.value}`;
+   return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = err => {
+   const message = `Duplicate field value: "${err.keyValue.name}"`;
+   return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = err => {
+   const errors = Object.values(err.errors).map(el => el.message);
+
+   const message = `Validation failed: ${errors.join('. ')}`;
+   return new AppError(message, 400);
+};
+
+const sendDevError = (err, res) => {
+   res.status(err.statusCode).json({
+      status: err.status,
+      error: err,
+      message: err.message,
+      stack: err.stack
+   });
+};
+
+const sendProdError = (err, res) => {
+   if (err.isOperational) {
+      res.status(err.statusCode).json({
+         status: err.status,
+         message: err.message
+      });
+   } else {
+      console.error('ERROR', err);
+
+      res.status(500).json({
+         status: 'error',
+         message: 'Something went wrong'
+      });
+   }
+};
+
 module.exports = (err, req, res, next) => {
-   console.log(err.stack);
    err.statusCode = err.statusCode || 500;
    err.status = err.status || 'error';
 
-   res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message
-   });
+   if (process.env.NODE_ENV === 'development') {
+      sendDevError(err, res);
+   } else if (process.env.NODE_ENV === 'production') {
+      let error = { ...err };
 
-   next();
+      if (err.name === 'CastError') error = handleCastErrorDB(error);
+      if (err.code === 11000) error = handleDuplicateFieldsDB(error);
+      if (err.name === 'ValidationError')
+         error = handleValidationErrorDB(error);
+      sendProdError(error, res);
+   }
 };
