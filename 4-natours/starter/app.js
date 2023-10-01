2e49d00995e61dbf0dda57b133b96a95f1ca4f9e
stevent12-rg <sturner@rategenius.com>
2023-10-01 09:28:58 -0500

    Section 10 - rpevent parameter pollution

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -4,6 +4,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const tourRouter = require('./route/tourRoutes');
 const userRouter = require('./route/userRoutes');
@@ -40,6 +41,20 @@ app.use(mongoSanitize());
 
 // Data sanitization against XSS
 app.use(xss());
+
+// Prevent request parameter pollution
+app.use(
+   hpp({
+      whitelist: [
+         'duration',
+         'ratingsAverage',
+         'ratingsQuantity',
+         'maxGroupSize',
+         'difficulty',
+         'price'
+      ]
+   })
+);
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
