7c7b2fc598673562b9c93139919e6825ab2c9bfa
stevent12-rg <sturner@rategenius.com>
2023-10-01 08:39:35 -0500

    Section 10 - implement helmet for headers

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const tourRouter = require('./route/tourRoutes');
 const userRouter = require('./route/userRoutes');
@@ -11,22 +12,29 @@ const globalErrorHandler = require('./controller/errorController');
 const app = express();
 
 // GLOBAL MIDDLEWARE
+// Set security HTTP headers
+app.use(helmet());
+
 console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
    app.use(morgan('dev'));
 }
 
+// Limit requests from same IP
 const limiter = rateLimit({
-   limit: 3,
+   limit: 100,
    windowMs: 60 * 60 * 1000,
    message: 'Request limit reached'
 });
 app.use('/api', limiter);
 
-app.use(express.json());
+// Body parser - read data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
-// Custom middleware
+// Custom test middleware
 app.use((req, res, next) => {
    req.requestTime = new Date().toISOString();
    next();
