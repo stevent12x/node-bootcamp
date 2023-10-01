4ba050b83f9066c15ab7d7c7bd016739ce9bb83d
stevent12-rg <sturner@rategenius.com>
2023-10-01 08:33:49 -0500

    Section 10 - implement rate limiting

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+
 const tourRouter = require('./route/tourRoutes');
 const userRouter = require('./route/userRoutes');
+
 const AppError = require('./util/appError');
 const globalErrorHandler = require('./controller/errorController');
 
 const app = express();
 
-// MIDDLEWARE
+// GLOBAL MIDDLEWARE
 console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
    app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+   limit: 3,
+   windowMs: 60 * 60 * 1000,
+   message: 'Request limit reached'
+});
+app.use('/api', limiter);
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
