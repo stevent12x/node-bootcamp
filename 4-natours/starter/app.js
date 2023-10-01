62f0ef6ef507c4d52644a5d35a01eed61ae1066e
stevent12-rg <sturner@rategenius.com>
2023-10-01 08:51:25 -0500

    Section 10 - more data sanitization

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const tourRouter = require('./route/tourRoutes');
 const userRouter = require('./route/userRoutes');
@@ -12,9 +14,11 @@ const globalErrorHandler = require('./controller/errorController');
 const app = express();
 
 // GLOBAL MIDDLEWARE
+
 // Set security HTTP headers
 app.use(helmet());
 
+// HTTP request logging, development only
 console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
    app.use(morgan('dev'));
@@ -30,6 +34,12 @@ app.use('/api', limiter);
 
 // Body parser - read data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
