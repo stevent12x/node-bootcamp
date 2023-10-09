496f7be20cf829dceac67b89c7155657b598fcc1
stevent12-rg <sturner@rategenius.com>
2023-10-09 09:17:40 -0500

    Section 11 - add indexing

--- 4-natours/starter/controller/handlerFactory.js
+++ 4-natours/starter/controller/handlerFactory.js
@@ -87,6 +87,7 @@ exports.getAll = Model =>
          .sort()
          .limitFields()
          .paginate();
+      // const doc = await features.query.explain;
       const doc = await features.query;
 
       // Send response
