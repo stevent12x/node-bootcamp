976e6c22ed3fcc970398c0008acf3baad54768f0
stevent12-rg <sturner@rategenius.com>
2023-10-11 09:42:18 -0500

    Section 12 - starting server-side rendering with pug template

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -14,6 +15,12 @@ const AppError = require('./util/appError');
 const globalErrorHandler = require('./controller/errorController');
 
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+// Serving static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Set security HTTP headers
 app.use(helmet());
@@ -55,9 +62,6 @@ app.use(
    })
 );
 
-// Serving static files
-app.use(express.static(`${__dirname}/public`));
-
 // Custom test middleware
 app.use((req, res, next) => {
    req.requestTime = new Date().toISOString();
@@ -65,6 +69,12 @@ app.use((req, res, next) => {
 });
 
 // ROUTES
+app.get('/', (req, res) => {
+   res.status(200).render('base', {
+      tour: 'The Forest Hiker',
+      user: 'stevent'
+   });
+});
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
