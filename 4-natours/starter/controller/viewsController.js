f1e1535aed458a4e2a79f9e0bf4061e979cd6082
stevent12x <steven.turner12x@gmail.com>
2023-10-12 09:47:16 -0500

    section 12 - building the tour page

--- 4-natours/starter/controller/viewsController.js
+++ 4-natours/starter/controller/viewsController.js
@@ -1,11 +1,29 @@
-exports.getOverview = (req, res) => {
-  res.status(200).render('overview', {
-    title: 'All Tours'
-  });
-};
+const Tour = require('../model/tourModel');
+const User = require('../model/userModel');
+const catchAsync = require('../util/catchAsync');
+const AppError = require('../util/appError');
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour'
+exports.getOverview = catchAsync(async (req, res, next) => {
+  const tours = await Tour.find();
+
+  res.status(200).render('overview', {
+    title: 'All Tours',
+    tours
   });
-};
+});
+
+exports.getTour = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user'
+  });
+
+  if (!tour) {
+    return next(new AppError('Tour not found', 400));
+  }
+
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker Tour',
+    tour
+  });
+});
